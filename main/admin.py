5b8ab2aa92a8eb641a54a2dc849bcae60e280fc3
Vasily.Gurtovoy <vasiliy.gurtovoy@cosmos2.ru>
2022-01-19 17:02:01 +0300

    Небольшие правки

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Vacancy, Resume, Contacts, Partners, ProductCategories, Products, Services
+from main.models import *
 
 
-@admin.register(Vacancy, Resume, Contacts, Partners, ProductCategories, Products, Services)
+@admin.register(Vacancy, Resume, Contacts, Partners, ProductCategories, Services)
 class CustomAdmin(admin.ModelAdmin):
     pass
 
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    filter_horizontal = ('categories',)
