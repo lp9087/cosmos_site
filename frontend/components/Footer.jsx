e33d94ac0abb34552571e51de4e05afa3e41ad4c
roman.salikov <roman.sergeevich.salikov@cosmos2.ru>
2022-02-04 10:35:17 +0300

    Добавлен футер

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -1,7 +1,52 @@
+import Link from 'next/link';
+
+import FOOTER_MOCK from '@/constants/mocks/footer';
+
 import { BaseScreen } from './screens';
 
 const Footer = () => {
-  return <BaseScreen container="w-full bg-sky-900 text-white">footer</BaseScreen>;
+  const { columns, email, phones, address, socials, copyright } = FOOTER_MOCK;
+
+  return (
+    <BaseScreen className="gap-16 pb-16" container="w-full bg-sky-900 text-white">
+      <div className="grid grid-cols-4 gap-6">
+        <div className="flex flex-col gap-4 row-start-1 col-start-4">
+          <h2 className="text-lg font-semibold">Свяжитесь с нами</h2>
+          <div className="flex flex-col gap-2">
+            <a href={`mailto:${email}`} className="underline">
+              {email}
+            </a>
+            {phones.map(x => (
+              <span key={x.id}>{x.phone}</span>
+            ))}
+          </div>
+          <div className="flex gap-3">
+            {socials.map(x => (
+              <a key={x.id} href={x.link} className="underline">
+                {x.name}
+              </a>
+            ))}
+          </div>
+        </div>
+        {columns.map(x => (
+          <div key={x.id} className="flex flex-col gap-4">
+            <h2 className="text-lg font-semibold">{x.title}</h2>
+            <div className="flex flex-col gap-2">
+              {x.items.map(x => (
+                <Link key={x.id} href={x.link} passHref>
+                  <a>{x.title}</a>
+                </Link>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="flex justify-between">
+        <span>&copy; {copyright}</span>
+        <address>{address}</address>
+      </div>
+    </BaseScreen>
+  );
 };
 
 export default Footer;
---
+++ frontend/constants/mocks/footer.ts
@@ -0,0 +1,42 @@
+const FOOTER_MOCK = {
+  columns: [
+    {
+      id: 1,
+      title: 'Продукты',
+      items: [
+        { id: 1, title: 'Продукт 1', link: '#' },
+        { id: 2, title: 'Продукт 2', link: '#' },
+      ],
+    },
+    {
+      id: 2,
+      title: 'Услуги',
+      items: [
+        { id: 1, title: 'Услуга 1', link: '#' },
+        { id: 2, title: 'Услуга 2', link: '#' },
+      ],
+    },
+    {
+      id: 3,
+      title: 'Компания',
+      items: [
+        { id: 1, title: 'О компании', link: '#' },
+        { id: 2, title: 'Сотрудничество', link: '#' },
+        { id: 3, title: 'Вакансии', link: '#' },
+      ],
+    },
+  ],
+  copyright: '2022 ООО "НПЦ "Космос-2"',
+  phones: [
+    { id: 1, phone: '+7 (888) 888-8888' },
+    { id: 2, phone: '+7 (999) 999-9999' },
+  ],
+  email: 'contact@cosmos2.ru',
+  address: 'Россия, г. Ростов-на-Дону, пер. Буденновский, 97',
+  socials: [
+    { id: 1, name: 'VK', link: '#' },
+    { id: 2, name: 'FB', link: '#' },
+  ],
+};
+
+export default FOOTER_MOCK;
